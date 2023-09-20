20fb7d715a7a3ce99300aae46acc0ad5860804a6
Jean Fellipe <jeanfellipesilva93@gmail.com>
2023-09-20 16:00:47 -0300

    add exercises 1.8, 1.9, 1.10, 1.11

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,5 +1,50 @@
 import { useState } from 'react'
 
+const Button = ({handleClick, text}) => {
+  return (
+  <button onClick={handleClick}>
+    {text}
+  </button>)
+}
+
+const StatisticsLine = ({text, value, isPercentage}) => {
+  return (
+    <tr><td>{text}</td><td>{value} {isPercentage}</td></tr>
+  )
+}
+
+const Statistics = ({good, neutral, bad, total, average, positive}) => {
+  /* const noFeedback = total == 0 */
+
+  if (total == 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>statistics</h2>
+
+      <table>
+        <tbody>
+          <StatisticsLine text='good' value={good} />
+          <StatisticsLine text='neutral' value={neutral} />
+          <StatisticsLine text='bad' value={bad} />
+          <StatisticsLine text='total' value={total} />
+          <StatisticsLine text='average' value={average} />
+          <StatisticsLine text='positive' value={positive} isPercentage='%' />
+        </tbody>
+
+      </table>
+
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -41,18 +86,12 @@ const App = () => {
   return (
     <div>
       <h2>give feedback</h2>
-      <button onClick={handleGood}>good</button>
-      <button onClick={handleNeutral}>neutral</button>
-      <button onClick={handleBad}>bad</button>
+      <Button handleClick={handleGood} text='good' />
+      <Button handleClick={handleNeutral} text='neutral' />
+      <Button handleClick={handleBad} text='bad' />
 
-      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive}/>
 
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
     </div>
   )
 }
